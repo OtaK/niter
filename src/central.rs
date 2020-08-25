7b37c2cf29243a59c874a17b6fef280324c2595c
Mathieu Amiot <amiot.mathieu@gmail.com>
2020-08-25 14:56:03 +0200

    WIP

--- src/central.rs
+++ src/central.rs
@@ -1,7 +1,7 @@
-pub trait Central<P> {
+pub trait Central<P: crate::Peripheral>: std::fmt::Debug {
     fn start_scan(&self) -> crate::NiterResult<()>;
-    fn filter_duplicates(&self) -> crate::NiterResult<()>;
     fn stop_scan(&self) -> crate::NiterResult<()>;
+    fn filter_duplicates(&mut self, filter: bool) -> crate::NiterResult<()>;
     fn peripherals(&self) -> crate::NiterResult<Vec<P>>;
-    fn get_peripheral(&self) -> crate::NiterResult<Option<P>>;
+    fn get_peripheral(&self, ) -> crate::NiterResult<Option<P>>;
 }
